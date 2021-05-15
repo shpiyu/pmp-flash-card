1040cfbd9291e6e1838632ba86038657434aeb7b
shpiyu <piyushranjan95@gmail.com>
2021-05-15 14:01:04 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pmp-flash-card
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/pmp-flash-card)
---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+// import { MatToolbarModule } from '@angular/material';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+
+@NgModule({
+  imports: [BrowserModule, FormsModule, MatToolbarModule],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
